de41685d64c7112ec31d458806f4bbd77395e365
Wallee <vladimshc@gmail.com>
2016-04-20 13:36:22 +0300

    тест clear 100% покрытие

--- src/test/ua/com/juja/mysqlcmd/controller/command/ClearTest.java
+++ src/test/ua/com/juja/mysqlcmd/controller/command/ClearTest.java
@@ -7,6 +7,8 @@ import ua.com.juja.mysqlcmd.model.DataSet;
 import ua.com.juja.mysqlcmd.model.DatabaseManager;
 import ua.com.juja.mysqlcmd.view.View;
 
+import java.util.IllegalFormatCodePointException;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
@@ -64,4 +66,31 @@ public class ClearTest {
         assertFalse(canProcess);
     }
 
+    @Test
+    public void testValidationErrorWhenCountParametersIsLessThan2() {
+        //when
+        try {
+            command.process("clear");
+            fail();
+        } catch (IllegalArgumentException e) {
+            //then
+            assertEquals("Command format 'clear|tableName', but you have: 'clear'", e.getMessage());
+
+        }
+
+    }
+
+    @Test
+    public void testValidationErrorWhenCountParametersIsMoreThan2() {
+        //when
+        try {
+            command.process("clear|table|qwe");
+            fail();
+        } catch (IllegalArgumentException e) {
+            //then
+            assertEquals("Command format 'clear|tableName', but you have: 'clear|table|qwe'", e.getMessage());
+
+        }
+
+    }
 }
